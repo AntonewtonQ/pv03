7b5718f772beb18a48f4591993ef37e1e62be907
AntonewtonQ <antonewtonquima@gmail.com>
2025-01-15 00:21:20 +0100

    adicionado shoplist

---
+++ src/components/ShopList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ShopList = () => {
+  return (
+    <div className="text-white px-6 md:px-14 py-6">
+      <div className="mx-auto px-10 max-w-6xl flex flex-col gap-6">
+        <div className="space-y-4 mb-2">
+          <h1 className="font-bold text-4xl">Tech Shirts Collection</h1>
+          <p className="text-muted-foreground max-w-2xl lg:text-xl">
+            Explore our collection of programming and tech-themed shirts. Find
+            the perfect shirt to express your passion for coding and technology.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopList;
